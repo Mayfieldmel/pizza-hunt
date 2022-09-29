1d9241a0c0d70e3936240290d1ba646836f731a8
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-09-29 17:37:38 -0400

    add model change

--- models/Pizza.js
+++ models/Pizza.js
@@ -20,7 +20,7 @@ const PizzaSchema = new Schema({
     size: {
         type: String,
         required: true,
-  enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
+        enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
         default: 'Large'
     },
     toppings: [],
