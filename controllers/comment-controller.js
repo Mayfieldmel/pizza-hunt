bee4fe7de07d9440e03676258e489bcdd0ebe222
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-09-29 16:35:34 -0400

    add reply controller/routes within comment

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -22,6 +22,23 @@ addComment({ params, body }, res) {
           .catch(err => res.json(err));
   },
 
+//   add reply
+addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
+
   // remove comment
   removeComment({ params }, res) {
     Comment.findOneAndDelete({ _id: params.commentId })
@@ -42,6 +59,17 @@ addComment({ params, body }, res) {
         }
         res.json(dbPizzaData);
       })
+      .catch(err => res.json(err));
+  },
+
+  // remove reply - api/comments/<commentId>/<replyId>
+removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => res.json(err));
   }
 };
